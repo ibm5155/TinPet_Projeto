d5a3bbdb92f6c0c265dea058327b926ff38144fe
ibm5155 <lucas-zimerman1@hotmail.com>
2017-10-18 15:58:49 -0200

    Login com o facebook no android OK (precisa terminar de portar para as outras plataformas)

--- TinPet_Projeto/TinPet_Projeto.Android/Tela_SemLogin.cs
+++ TinPet_Projeto/TinPet_Projeto.Android/Tela_SemLogin.cs
@@ -9,9 +9,11 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Xamarin.Auth;
+using TinPet_Projeto.APIS;
 
 using TinPet_Projeto;
-
+using Newtonsoft.Json.Linq;
 
 namespace TinPet_Projeto.Droid
 {
@@ -29,7 +31,39 @@ namespace TinPet_Projeto.Droid
             Button BotaoProxTela = FindViewById<Button>(Resource.Id.Botao_Login);
             BotaoProxTela.Click += delegate
             {
-                StartActivity(typeof(Tela_SelecionaAnimais));
+
+                var auth = new OAuth2Authenticator(
+            clientId: "537904446553253",//513f5038d6c8721421782ce592e5cec7
+            scope: "",
+            authorizeUrl: new Uri("https://m.facebook.com/v2.10/dialog/oauth/"),
+            redirectUrl: new Uri("http://www.facebook.com/connect/login_success.htm"));
+
+             //   var auth = PCLFacebook.GetAutenticador();
+                var ui = auth.GetUI(this);
+
+
+                auth.Completed += async (sender, eventArgs) => {
+                    if (eventArgs.IsAuthenticated)
+                    {
+                        var accessToken = eventArgs.Account.Properties["access_token"].ToString();
+                        var expiresIn = Convert.ToDouble(eventArgs.Account.Properties["expires_in"]);
+                        var expiryDate = DateTime.Now + TimeSpan.FromSeconds(expiresIn);
+
+                        var request = new OAuth2Request("GET", new Uri("https://graph.facebook.com/me"), null, eventArgs.Account);
+                        var response = await request.GetResponseAsync();
+                        var obj = JObject.Parse(response.GetResponseText());
+
+                        var id = obj["id"].ToString().Replace("\"", "");
+                        var name = obj["name"].ToString().Replace("\"", "");
+                    }
+                    else
+                    {
+                        // o usuario cancelou o login
+                    }
+                };
+
+                StartActivity(auth.GetUI(this));
+                //StartActivity(typeof(Tela_SelecionaAnimais));
             };
         }
     };
---
+++ TinPet_Projeto/TinPet_Projeto/APIS/iFacebook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Auth;
+
+namespace TinPet_Projeto.APIS
+{
+    public class PCLFacebook
+    {
+        /*https://components.xamarin.com/gettingstarted/xamarin.auth*/
+        public static OAuth2Authenticator GetAutenticador()
+        {
+            var auth = new OAuth2Authenticator(
+            clientId: "537904446553253",//513f5038d6c8721421782ce592e5cec7
+            scope: "",
+            authorizeUrl: new Uri("https://m.facebook.com/dialog/oauth/"),
+            redirectUrl: new Uri("http://www.facebook.com/connect/login_success.html"));
+            return auth;
+        }
+    }
+}
