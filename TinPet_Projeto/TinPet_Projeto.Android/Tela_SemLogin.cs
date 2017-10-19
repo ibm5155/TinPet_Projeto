6e31129f4e13cc0a61c300ea03d63a456c71511f
ibm5155 <lucas-zimerman1@hotmail.com>
2017-10-19 11:40:18 -0200

    Codigo para pegar nome/ID exportado para classe portatil

--- TinPet_Projeto/TinPet_Projeto.Android/Tela_SemLogin.cs
+++ TinPet_Projeto/TinPet_Projeto.Android/Tela_SemLogin.cs
@@ -13,7 +13,8 @@ using Xamarin.Auth;
 using TinPet_Projeto.APIS;
 
 using TinPet_Projeto;
-using Newtonsoft.Json.Linq;
+using TinPet_Projeto.APIS;
+using System.Threading.Tasks;
 
 namespace TinPet_Projeto.Droid
 {
@@ -31,39 +32,60 @@ namespace TinPet_Projeto.Droid
             Button BotaoProxTela = FindViewById<Button>(Resource.Id.Botao_Login);
             BotaoProxTela.Click += delegate
             {
+                /*
+                                var auth = new OAuth2Authenticator(
+                            clientId: "537904446553253",//513f5038d6c8721421782ce592e5cec7
+                            scope: "",
+                            authorizeUrl: new Uri("https://m.facebook.com/v2.10/dialog/oauth/"),
+                            redirectUrl: new Uri("http://www.facebook.com/connect/login_success.htm"));
 
-                var auth = new OAuth2Authenticator(
-            clientId: "537904446553253",//513f5038d6c8721421782ce592e5cec7
-            scope: "",
-            authorizeUrl: new Uri("https://m.facebook.com/v2.10/dialog/oauth/"),
-            redirectUrl: new Uri("http://www.facebook.com/connect/login_success.htm"));
-
-             //   var auth = PCLFacebook.GetAutenticador();
-                var ui = auth.GetUI(this);
+                             //   var auth = PCLFacebook.GetAutenticador();
+                                var ui = auth.GetUI(this);
 
 
-                auth.Completed += async (sender, eventArgs) => {
-                    if (eventArgs.IsAuthenticated)
+                                auth.Completed += async (sender, eventArgs) => {
+                                    if (eventArgs.IsAuthenticated)
+                                    {
+                                        var accessToken = eventArgs.Account.Properties["access_token"].ToString();
+                                        var expiresIn = Convert.ToDouble(eventArgs.Account.Properties["expires_in"]);
+                                        var expiryDate = DateTime.Now + TimeSpan.FromSeconds(expiresIn);
+
+                                        var request = new OAuth2Request("GET", new Uri("https://graph.facebook.com/me"), null, eventArgs.Account);
+                                        var response = await request.GetResponseAsync();
+                                        var obj = JObject.Parse(response.GetResponseText());
+
+                                        var id = obj["id"].ToString().Replace("\"", "");
+                                        var name = obj["name"].ToString().Replace("\"", "");
+                                    }
+                                    else
+                                    {
+                                        // o usuario cancelou o login
+                                    }
+                                };
+                                */
+                PCLFacebook Usuario = new PCLFacebook();
+                var ui = Usuario.auth.GetUI(this);
+                Task task = new Task( ()=>
+                {
+                    //Roda em paralelo a UI principal o código para pegar a ID/NOME do usuário
+                    Usuario.GetID();
+                    StartActivity(ui);
+                    Usuario.semaforo.WaitOne(); //espera a tela do facebook terminar de ser processada
+                    //não botar o wait one fora da thread senão você vai travar a main thread que lida com a ui em geral
+                                                //terminado o processo, checar o que aconteceu
+                    if (Usuario.Id != "")
                     {
-                        var accessToken = eventArgs.Account.Properties["access_token"].ToString();
-                        var expiresIn = Convert.ToDouble(eventArgs.Account.Properties["expires_in"]);
-                        var expiryDate = DateTime.Now + TimeSpan.FromSeconds(expiresIn);
+                        //Temos um usuário válido
+                        StartActivity(typeof(Tela_SelecionaAnimais));
 
-                        var request = new OAuth2Request("GET", new Uri("https://graph.facebook.com/me"), null, eventArgs.Account);
-                        var response = await request.GetResponseAsync();
-                        var obj = JObject.Parse(response.GetResponseText());
-
-                        var id = obj["id"].ToString().Replace("\"", "");
-                        var name = obj["name"].ToString().Replace("\"", "");
                     }
                     else
                     {
-                        // o usuario cancelou o login
+                        //Usuario cancelou
                     }
-                };
 
-                StartActivity(auth.GetUI(this));
-                //StartActivity(typeof(Tela_SelecionaAnimais));
+                });
+                task.Start();
             };
         }
     };
--- TinPet_Projeto/TinPet_Projeto/APIS/iFacebook.cs
+++ TinPet_Projeto/TinPet_Projeto/APIS/iFacebook.cs
@@ -2,22 +2,63 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Auth;
+using Newtonsoft.Json.Linq;
 
 namespace TinPet_Projeto.APIS
 {
     public class PCLFacebook
     {
+        public Semaphore semaforo = new Semaphore(0, 1);
+        public OAuth2Authenticator auth = null;
+        string accessToken;
+        double ExpiraEm;
+        DateTime DataExpirar;
+        public string Id;
+        public string Nome;
+
         /*https://components.xamarin.com/gettingstarted/xamarin.auth*/
-        public static OAuth2Authenticator GetAutenticador()
+        public PCLFacebook()
         {
-            var auth = new OAuth2Authenticator(
+            Id = "";
+            Nome = "";
+            accessToken = "";
+            ExpiraEm = 0;
+
+            auth = new OAuth2Authenticator(
             clientId: "537904446553253",//513f5038d6c8721421782ce592e5cec7
             scope: "",
-            authorizeUrl: new Uri("https://m.facebook.com/dialog/oauth/"),
-            redirectUrl: new Uri("http://www.facebook.com/connect/login_success.html"));
-            return auth;
+            authorizeUrl: new Uri("https://m.facebook.com/v2.10/dialog/oauth/"),
+            redirectUrl: new Uri("http://www.facebook.com/connect/login_success.htm"));
+        }
+
+        public void GetID()
+        {
+            auth.Completed += async (sender, eventArgs) =>
+            {
+
+                if (eventArgs.IsAuthenticated)
+                {
+                    accessToken = eventArgs.Account.Properties["access_token"].ToString();
+                    ExpiraEm = Convert.ToDouble(eventArgs.Account.Properties["expires_in"]);
+                    DataExpirar = DateTime.Now + TimeSpan.FromSeconds(ExpiraEm);
+
+                    var request = new OAuth2Request("GET", new Uri("https://graph.facebook.com/me"), null, eventArgs.Account);
+                    var response = await request.GetResponseAsync();
+                    var obj = JObject.Parse(response.GetResponseText());
+
+                    Id = obj["id"].ToString().Replace("\"", "");
+                    Nome = obj["name"].ToString().Replace("\"", "");
+                    semaforo.Release();
+                }
+                else
+                {
+                    // o usuario cancelou o login
+                    semaforo.Release();
+                }
+            };
         }
     }
 }
